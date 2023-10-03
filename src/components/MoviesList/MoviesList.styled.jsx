ac34756d05ed6aa7e1dd17d414e874ad662120c6
IgorMushk <igor.mushketik@gmail.com>
2023-10-03 11:26:55 +0300

    Add Select period trending + styles MovieList

--- src/components/MoviesList/MoviesList.styled.jsx
+++ src/components/MoviesList/MoviesList.styled.jsx
@@ -33,17 +33,34 @@ const ImgItem = styled.img`
   transition: transform 300ms cubic-bezier(0.4, 0, 0.2, 1);
   &:hover {
   transform: scale(1.02);
-  cursor: zoom-in;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  //cursor: zoom-in;
   }
-
 `;
-
 
 const MovieTitle =styled.h2`
 display: inline;
 overflow-wrap: break-word;
 font-size: 18px;
 font-weight: 400;
-`
+`;
 
-export { Title, List, MovieItem, MovieTitle, ImgItem };
+const WrapperSelectTrend =styled.div`
+display:  flex;
+flex-wrap: wrap;
+justify-content: space-between;
+align-items: center;
+//padding-right: 15px;
+`;
+
+const SelectCustom = styled.select`
+margin-right: 30px;
+width: 70px;
+height: 30px;
+background-color: #032541;
+color:  #ffc107;
+border: none;
+border-radius: 4px;
+`;
+
+export { Title, List, MovieItem, MovieTitle, ImgItem, WrapperSelectTrend, SelectCustom };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,42 @@
 import MoviesList from 'components/MoviesList/MoviesList';
-import { Title } from 'components/MoviesList/MoviesList.styled';
+import { SelectCustom, Title, WrapperSelectTrend } from 'components/MoviesList/MoviesList.styled';
 import React, { useEffect, useState } from 'react';
 import { fetchMovies } from 'services/moviesAPI';
 
 const Home = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
-  
+  const [period, setPeriod] = useState('day');
+
   useEffect(() => {
     const fetchTrendingMovies = async () => {
       try {
-        const data = await fetchMovies('/trending/movie/day?language=en-US');
+        const data = await fetchMovies(
+          `/trending/movie/${period}?language=en-US`
+        );
         setTrendingMovies(data.results);
       } catch (err) {
         console.log(err);
       }
     };
     fetchTrendingMovies();
-  }, []);
+  }, [period]);
+  const onSelectChange = evt => {
+    setPeriod(evt.target.value);
+  };
+
   return (
     <>
-      <Title>Trending today</Title>
+      <WrapperSelectTrend>
+        {period === 'day' ? (
+          <Title>Trending today</Title>
+        ) : (
+          <Title>Trending for a week</Title>
+        )}
+        <SelectCustom value={period} onChange={onSelectChange}>
+          <option value="day">Day</option>
+          <option value="week">Week</option>
+        </SelectCustom>
+      </WrapperSelectTrend>
       <MoviesList movies={trendingMovies} />
     </>
   );
